8d9d88af6cc97697cc1ec83b485d549a41100a2a
muhghalib <muhamadghalib7@gmail.com>
2023-11-12 20:37:27 +0700

    feat(constant): add footer content

---
+++ src/constant/contents/footer.ts
@@ -0,0 +1,81 @@
+export const FOOTER_ABOUT =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam';
+
+export const FOOTER_EMAIL = ' info@jstemplate.net';
+export const FOOTER_PHONE_NUMBER = '880 123 456 789';
+
+export const FOOTER_LIST = [
+  {
+    title: 'Quick Link',
+    children: [
+      {
+        label: 'Home',
+        url: '/',
+      },
+      {
+        label: 'About',
+        url: '/about',
+      },
+      {
+        label: 'Blog',
+        url: '/blog',
+      },
+      {
+        label: 'Archived',
+        url: '/archived',
+      },
+      {
+        label: 'Author',
+        url: '/author',
+      },
+      {
+        label: 'Contact',
+        url: '/Contact',
+      },
+    ],
+  },
+  {
+    title: 'Category',
+    children: [
+      {
+        label: 'Lifestyle',
+        url: '/lifestyle',
+      },
+      {
+        label: 'Technology',
+        url: '/technology',
+      },
+      {
+        label: 'Travel',
+        url: '/travel',
+      },
+      {
+        label: 'Business',
+        url: '/business',
+      },
+      {
+        label: 'Economy',
+        url: '/economy',
+      },
+      {
+        label: 'Sport',
+        url: '/sport',
+      },
+    ],
+  },
+];
+
+export const FOOTER_INFO = [
+  {
+    label: 'Terms of Use',
+    url: '/terms-of-use',
+  },
+  {
+    label: 'Privacy Policy',
+    url: '/privacy-policy',
+  },
+  {
+    label: 'Cookie Policy',
+    url: '/cookie-policy',
+  },
+];
