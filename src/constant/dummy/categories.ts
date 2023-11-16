8f9f504fdbf856cd59fb858212ed177552dda663
muhghalib <muhamadghalib7@gmail.com>
2023-11-16 23:06:06 +0700

    feat: add categories dummy data

---
+++ src/constant/dummy/categories.ts
@@ -0,0 +1,14 @@
+export const DUMMY_CATEGORIES: Category[] = [
+  {
+    id: '1',
+    name: 'Technology',
+    createdAt: '2023-01-01',
+    updatedAt: '2023-01-01',
+  },
+  {
+    id: '2',
+    name: 'Travel',
+    createdAt: '2023-01-02',
+    updatedAt: '2023-01-02',
+  },
+];
