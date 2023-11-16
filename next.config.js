aa4bfbd3e369b09727a25a7ede89d1975c7ced41
muhghalib <muhamadghalib7@gmail.com>
2023-11-16 23:10:19 +0700

    config(next): add images remote patterns to unsplash, and permanent redirects from '/' to '/home'

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  },
+  redirects: async () => [
+    {
+      source: '/',
+      destination: '/home',
+      permanent: true,
+      statusCode: 302,
+    },
+  ],
+};
 
 module.exports = nextConfig;
