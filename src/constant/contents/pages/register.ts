ae319db73727885db1e62a2ed9d2f092a20da8e6
ghalib_24 <muhamadghalib7@gmail.com>
2023-12-03 22:28:02 +0700

    feat(constant): add content for register and login pages

---
+++ src/constant/contents/pages/register.ts
@@ -0,0 +1,12 @@
+import { Facebook, Github } from 'lucide-react';
+
+export const REGISTER_GREETING = 'Access 5,674,040 free blog post with us';
+
+export const REGISTER_WITH_ACTION = [
+  {
+    icon: Github,
+  },
+  {
+    icon: Facebook,
+  },
+];
