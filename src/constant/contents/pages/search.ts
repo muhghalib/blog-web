7d34ca09bef22c02f37a1b23348055b1dcdac9db
ghalib_24 <muhamadghalib7@gmail.com>
2023-12-13 13:28:04 +0700

    feat: add search sort by option constant

---
+++ src/constant/contents/pages/search.ts
@@ -0,0 +1,14 @@
+export const SEARCH_SORT_BY_OPTION = [
+  {
+    label: 'Latest',
+    value: 'latest',
+  },
+  {
+    label: 'Oldest',
+    value: 'oldest',
+  },
+  {
+    label: 'Popular',
+    value: 'popular',
+  },
+];
