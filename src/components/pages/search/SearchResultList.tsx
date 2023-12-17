5489686f61a16c51a6bdec7c6b09c4a00aac4836
ghalib_24 <muhamadghalib7@gmail.com>
2023-12-17 10:06:17 +0700

    feat: change description props to content props

--- src/components/pages/search/SearchResultList.tsx
+++ src/components/pages/search/SearchResultList.tsx
@@ -6,20 +6,22 @@ export const SearchResultList = ({ blogData }: { blogData: Blog[] }) => {
   return (
     <Section className="flex">
       <Grid className="w-full gap-8" cols={1} sm={2} md={3}>
-        {blogData.map(({ id, author, thumbnail, categories, description, title, createdAt }) => {
-          return (
-            <BlogCard
-              key={id}
-              id={id}
-              author={author}
-              thumbnail={thumbnail}
-              categories={categories}
-              title={title}
-              description={description}
-              createdAt={createdAt}
-            />
-          );
-        })}
+        {blogData.map(
+          ({ id, author, thumbnail, categories, description, title, content, createdAt }) => {
+            return (
+              <BlogCard
+                key={id}
+                id={id}
+                author={author}
+                thumbnail={thumbnail}
+                categories={categories}
+                title={title}
+                content={content}
+                createdAt={createdAt}
+              />
+            );
+          },
+        )}
       </Grid>
     </Section>
   );
